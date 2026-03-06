6e596b3d6434a0b8d9c3c16a95e8073a66787f14
gould7789 <hyeon.woo7789@gmail.com>
2026-03-06 22:24:10 +0900

    fillableを追加 tinkerの操作

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    // fillalbe: Product::create([...]) で一括代入できるカラムを指定するもの
+    // セキュリティ対策として、明示的に許可したカラムだけ代入可能にする
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'is_published',
+        'category_id'
+    ];
 }
